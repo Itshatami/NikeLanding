9859ce4ac2b01a9b34e30c57809de92620a857c9
alireza hatami <itshatami@gmail.com>
2024-08-20 05:01:49 +0330

    adding icons,images,constants

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import { headerLogo } from "../assets/images";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={30} />
+              </a>
+              <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                   
+              </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
